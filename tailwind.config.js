8f88dcb7d1e64b069f4f17bd1e69e078db980a80
Shaik Abdul Nabi <abdulnabi4700@gmail.com>
2025-07-28 13:57:24 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgDark: "#181C14",
+        accent1: "#3C3D37",
+        accent2: "#697565",
+        textLight: "#ECDFCC",
+      },
+    },
   },
   plugins: [],
-}
+};
